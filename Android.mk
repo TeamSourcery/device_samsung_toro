3138060fba620e5a66832e7ed6a161a3430cfff8
Eagle1967 <icsourcery@gmail.com>
2012-05-12 01:23:14 -0600

    Update to IMM76D and merge android-4.0.4_r1.1

--- Android.mk
+++ Android.mk
@@ -22,7 +22,15 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(TARGET_DEVICE), toro)
+
+ifneq ($(TARGET_SIMULATOR),true)
+
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
+
+endif

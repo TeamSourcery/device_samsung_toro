3b0d232a4a5c3f0bb25871a9390c450441bc947a
Eagle1967 <icsourcery@gmail.com>
2012-07-17 00:30:19 -0600

    remove tethering provision, fix cdma eri, fix battery compacity, build gallery 2 not 1 as it builds camera--peteralfonso,cm,teamsourcery

--- full_toro.mk
+++ full_toro.mk
@@ -23,7 +23,7 @@
 
 # Camera and Gallery
 PRODUCT_PACKAGES := \
-    Gallery
+    Gallery2
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
